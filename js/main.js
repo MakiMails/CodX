0681a4f99928e7d0abbc80e694e227e38d15d511
MakiMails <salivonhik1990@gmail.com>
2023-05-26 08:59:09 +0300

    _

--- js/main.js
+++ js/main.js
@@ -1,106 +1,115 @@
-function setCustomsValidityForm(){
-  let form = document.forms['forms-subs-events'];
+//форма "Sing Up"
+function setCustomsValidityForm() {
+  let form = document.forms["forms-subs-events"];
 
-  let inputEmail = form.elements['emailUser'];
-  inputEmail.setCustomValidity('This field should not be empty');
-  inputEmail.addEventListener('input', onInputEmail);
-  
-  let inputPhoneNum = form.elements['prohoneNumUser'];
-  inputPhoneNum.setCustomValidity('This field should not be empty');
-  inputPhoneNum.addEventListener("input",onInputPhoneNum);
-  
+  let inputEmail = form.elements["emailUser"];
+  inputEmail.setCustomValidity("This field should not be empty");
+  inputEmail.addEventListener("input", onInputEmail);
+
+  let inputPhoneNum = form.elements["prohoneNumUser"];
+  inputPhoneNum.setCustomValidity("This field should not be empty");
+  inputPhoneNum.addEventListener("input", onInputPhoneNum);
 }
 
-function onInputEmail(event){
+function onInputEmail(event) {
   let inputEmail = event.target;
-  cuctomValidEmail(inputEmail)
+  cuctomValidEmail(inputEmail);
 }
 
-function onInputPhoneNum(event){
+function onInputPhoneNum(event) {
   let inputPhoneNum = event.target;
   cuctomValidPhoneNum(inputPhoneNum);
 }
 
-function cuctomValidEmail(inputEmail){
+function cuctomValidEmail(inputEmail) {
   let emailPattern = /\S+@\S+\.\S+/;
 
-  if(inputEmail.value != ''){
+  if (inputEmail.value != "") {
     if (!emailPattern.test(inputEmail.value)) {
-      inputEmail.setCustomValidity('The email address was entered incorrectly');
-    } 
-    else{
-      inputEmail.setCustomValidity('');
-    } 
-  }else{
-    inputEmail.setCustomValidity('This field should not be empty');
+      inputEmail.setCustomValidity("The email address was entered incorrectly");
+    } else {
+      inputEmail.setCustomValidity("");
+    }
+  } else {
+    inputEmail.setCustomValidity("This field should not be empty");
   }
 }
 
-function cuctomValidPhoneNum(inputPhoneNum){
+function cuctomValidPhoneNum(inputPhoneNum) {
   let phonePattern = /^(\+375|80)(29|25|44|33)(\d{3})(\d{2})(\d{2})$/;
 
-  if(inputPhoneNum.value != ''){
+  if (inputPhoneNum.value != "") {
     if (!phonePattern.test(inputPhoneNum.value)) {
-      inputPhoneNum.setCustomValidity('Phone number entered incorrectly');
-    } 
-    else{
-      inputPhoneNum.setCustomValidity('');
+      inputPhoneNum.setCustomValidity("Phone number entered incorrectly");
+    } else {
+      inputPhoneNum.setCustomValidity("");
     }
-  }else{
-    inputPhoneNum.setCustomValidity('This field should not be empty');
+  } else {
+    inputPhoneNum.setCustomValidity("This field should not be empty");
   }
 }
 
+//Модальное окно "Отзывы"
 
+const modalFeedback = document.getElementById("modal-feedback");
+const modalFeedbackWrap = modalFeedback.querySelector(".modal__wrap");
+const bntCloseModalFeedback = modalFeedbackWrap.querySelector(".bnt-close");
+const formFeedback = modalFeedbackWrap.querySelector("#modal-feedback__form");
+const inputUserName = formFeedback.elements["userName"];
+const textareaFeedbackText = formFeedback.elements["feedbackText"];
 
-
-function setEventBntOpenModalFeedback(){
-  const bntOpenModelFeedback = document.getElementById('bnt-open-modal-feedback');
-  bntOpenModelFeedback.addEventListener('click', onClickBntOpenModalFeedback)
+function removeEventsOnFeedbackModelWindow() {
+  modalFeedback.removeEventListener("click", onClickModalFeedback);
+  bntCloseModalFeedback.removeEventListener(
+    "click",
+    onClickBntCloseModalFeedback
+  );
+  document.removeEventListener("keydown", onDownKeyEscape);
 }
 
-function onClickBntOpenModalFeedback(){
+function clearFieldFormFeedback() {
+  inputUserName.textValue = "";
+}
+
+function closeModalFeedback() {
+  removeEventsOnFeedbackModelWindow();
+  modalFeedback.classList.add("modal-hidden");
+  clearFieldFormFeedback();
+}
+
+function onClickModalFeedback(evt) {
+  if (evt.target == modalFeedback) {
+    closeModalFeedback();
+  }
+}
+
+function onClickBntCloseModalFeedback(evt) {
+  closeModalFeedback();
+}
+
+function onDownKeyEscape(evt) {
+  if (evt.key === "Escape") {
+    evt.preventDefault();
+    closeModalFeedback();
+  }
+}
+
+function setEventsOnModalFeedback() {
+  modalFeedback.addEventListener("click", onClickModalFeedback);
+  bntCloseModalFeedback.addEventListener("click", onClickBntCloseModalFeedback);
+  document.addEventListener("keydown", onDownKeyEscape);
+}
+
+function showModalFeedback() {
+  setEventsOnModalFeedback();
+  modalFeedback.classList.remove("modal-hidden");
+}
+
+function onClickBntOpenModalFeedback() {
   showModalFeedback();
 }
 
-function showModalFeedback(){
-  setEventsOnModalFeedback();
-  modalFeedback.classList.remove('modal-hidden');
-}
-
-function setEventsOnModalFeedback(){
-  modalFeedback.addEventListener('click', onClickModalFeedback);
-  document.addEventListener('keydown', onDownKeyEscape);
-}
-
-function onClickModalFeedback(event){
-  const form = modalFeedback.querySelector('#modal-feedback__form');
-  const bntCloseModal = form.elements['bntClose'];
-  if(event.target === bntCloseModal || event.target === modalFeedback){
-    closeModalFeedback();
-  }
-}
-
-function onDownKeyEscape(event){
-  if(event.key === 'Escape'){
-    event.preventDefault();
-    closeModalFeedback();
-  }
-}
-
-function closeModalFeedback(){
-  removeEventsOnFeedbackModelWindow();
-  modalFeedback.classList.add('modal-hidden');
-}
-
-function removeEventsOnFeedbackModelWindow(){
-  modalFeedback.removeEventListener('click', onClickModalFeedback);
-  document.removeEventListener('keydown',onDownKeyEscape);
-}
-
-const modalFeedback = document.getElementById('modal-feedback');
-
-
+const bntOpenModelFeedback = document.getElementById("bnt-open-modal-feedback");
+bntOpenModelFeedback.addEventListener("click", onClickBntOpenModalFeedback);
 
 setCustomsValidityForm();
-setEventBntOpenModalFeedback();
